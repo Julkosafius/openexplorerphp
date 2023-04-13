e94702961506e9b04f3bf7911acc8b63086fb618
Julkosafius <kuhnjulian.jk@gmail.com>
2023-04-13 22:36:02 +0200

    copy bug fixing

--- action_copy.php
+++ action_copy.php
@@ -10,16 +10,8 @@ $destination = isset($_POST['destination']) ? $_POST['destination'] : null;
 $files = isset($_POST['files']) ? $_POST['files'] : null;
 $folders = isset($_POST['folders']) ? $_POST['folders'] : null;
 
-if (!$destination) {
-    $status_array[] = 'No destination folder found.';
-    echo json_encode($status_array);
-    die();
-}
-
-$destination_name = $sqlite->getFirstColumnValue('select folder_name as fn from folders where rowid = '.$destination, 'fn');
-
 function copyFile($file_id, $parent_folder_id = 0) {
-    global $sqlite, $destination;
+    global $sqlite, $destination, $status_array;
     $file_info = getInfo($file_id, 'files');
     $folder_id = empty($parent_folder_id) ? $destination : $parent_folder_id;
     $sqlite->executeCommands('insert into files(file_name, file_type, file_size, file_time, file_hash, folder_id) '
@@ -31,18 +23,20 @@ function copyFile($file_id, $parent_folder_id = 0) {
                             .$file_info['file_hash'].'", '
                             .$folder_id
                             .')');
+    $status_array[] = 'Copied file '.$file_info['file_name'].'.'.$file_info['file_type'].'.';
 }
 
-
 function copyFolder($folder_id, $last_folder_id = 0) {
-    global $sqlite, $destination;
+    global $sqlite, $destination, $status_array;
     $folder_contents = getFolderContents($folder_id);
+    // the contents of the original folder
     $elements = array_merge($folder_contents['folders'], $folder_contents['files']);
     if (!empty($elements)) {
         for ($i = 0; $i < count($elements); $i++) {
             if (isset($elements[$i]['folder_id'])) {
                 $folder_info = getInfo($elements[$i]['folder_id'], 'folders');
                 $parent_folder_id = empty($folder_info['parent_folder_id']) ? $destination : $last_folder_id;
+                // create a new folder with the same stats (updated timestamp)
                 $sqlite->executeCommands('insert into folders(user_id, folder_name, parent_folder_id, folder_time, folder_size) values ("'
                                         .$folder_info['user_id'].'", "'
                                         .$folder_info['folder_name'].'", '
@@ -50,9 +44,16 @@ function copyFolder($folder_id, $last_folder_id = 0) {
                                         .time().', '
                                         .$folder_info['folder_size']
                                         .')');
+                // get the new id of the just created folder to copy the new recursively found files
+                // from the original folder to the new one
                 $new_last_folder_id = $sqlite->getFirstColumnValue('select max(rowid) as id from folders where parent_folder_id ='.$parent_folder_id, 'id');
+                if (!empty($new_last_folder_id)) {
+                    $status_array[] = 'Copied folder '.$folder_info['folder_name'].'.';
+                    $subfolder_contents = copyFolder($elements[$i]['folder_id'], $new_last_folder_id);
+                } else {
+                    $status_array[] = 'Could not copy '.$folder_info['folder_name'].'.';
+                }
 
-                $subfolder_contents = copyFolder($elements[$i]['folder_id'], $new_last_folder_id);
             } else {
                 copyFile($elements[$i]['file_id'], $last_folder_id);
             }
@@ -60,11 +61,28 @@ function copyFolder($folder_id, $last_folder_id = 0) {
     }
 }
 
+if (!$destination) {
+    $status_array[] = 'No destination folder found.';
+    echo json_encode($status_array);
+    die();
+}
+
+$destination_name = $sqlite->getFirstColumnValue('select folder_name as fn from folders where rowid = '.$destination, 'fn');
+
 if (!empty($folders)) {
     foreach($folders as $folder_id) {
+        // create a copy of the selected folder in the destination folder 
         $folder_info = getInfo($folder_id, 'folders');
-        
-        copyFolder($folder_id, $destination);
+        $sqlite->executeCommands('insert into folders(user_id, folder_name, parent_folder_id, folder_time, folder_size) values ("'
+                                .$folder_info['user_id'].'", "'
+                                .$folder_info['folder_name'].'", '
+                                .$destination.', '
+                                .time().', '
+                                .$folder_info['folder_size']
+                                .')');
+        $new_folder_id = $sqlite->getFirstColumnValue('select max(rowid) as id from folders where parent_folder_id ='.$destination, 'id');
+        // copy all contents of the original selected folder in his copy
+        copyFolder($folder_id, $new_folder_id);
     }
 }
 
@@ -73,3 +91,5 @@ if (!empty($files)) {
         copyFile($file_id, $destination);
     }
 }
+
+echo json_encode($status_array);
